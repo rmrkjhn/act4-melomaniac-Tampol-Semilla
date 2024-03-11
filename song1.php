8dd109fa8164388e40a84a04f96f9b92f98954c1
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:09:03 +0800

    song1.php

---
+++ song1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>First Song</title>
+</head>
+<body>
+   <h1>Pano by Zack Tabudlo</h1>
+<?php
+    echo "<pre>";
+    echo "[Verse 1]\n";
+    echo "           Cadd9\n";
+    echo "oh giliw naririnig mo ba\n";
+    echo "        Em7\n";
+    echo "ang yong sarili\n";
+    echo "          Am7\n";
+    echo "nakakabaliw lumalabas\n";
+    echo "           F\n";
+    echo "sa yong bibig\n";
+    echo "           Cadd9\n";
+    echo "alam kong uto uto ako\n";
+    echo "          Em7\n";
+    echo "alam ko na marupok\n";
+    echo "         Am7\n";
+    echo "tao lang din naman\n";
+    echo "        F\n";
+    echo "kasi ako\n\n";
+
+    echo "[Pre-Chorus]\n";
+    echo "\n";
+    echo "   Am7                 Em7\n";
+    echo "may nararamdaman din ako\n";
+    echo "          F\n";
+    echo "di kasi manhid na tulad mo\n";
+    echo "             Am7\n";
+    echo "alam kong sanay bumitaw\n";
+    echo "   Em7\n";
+    echo "ang isang tulad mo\n";
+    echo "Cadd9           F\n";
+    echo "lalayo na ba ako\n\n";
+
+    echo "[Chorus]\n";
+    echo "      Cadd9\n";
+    echo "pano naman ako\n";
+    echo "         Em7\n";
+    echo "nahulog na sayo\n";
+    echo "   Am7                        F\n";
+    echo "binitawan mo lang ba talaga ako\n\n";
+
+    echo "        Cadd9\n";
+    echo "pano naman ako\n";
+    echo "        Em7\n";
+    echo "naghintay ng matagal sayo\n";
+    echo "           Am7                     F\n";
+    echo "wala lang ba talaga lahat ng yon sayo\n";
+    echo "                     Cadd9  Em7 Am7 F\n";
+    echo "ano na bang gagawin ko\n\n";
+
+    echo "[Verse 2]\n";
+    echo "           Cadd9\n";
+    echo "sinasadya mo ba ang lahat\n";
+    echo "               Em7\n";
+    echo "o trip mo lang ba ako saktan\n";
+    echo "           Am\n";
+    echo "pagtapos kong ibigay balikat ko\n";
+    echo "     F\n";
+    echo "pag ika'y umiiyak\n\n";
+
+    echo "            Cadd9\n";
+    echo "ano bang tingin mo saakin\n";
+    echo "          Em7\n";
+    echo "isa ba akong alipin\n";
+    echo "              Am7\n";
+    echo "wala ka bang modo\n";
+    echo "anong ginawa mo\n";
+    echo "   F\n";
+    echo "nagtiwala naman sayo\n\n";
+
+    echo "[Pre-Chorus]\n";
+    echo "\n";
+    echo "   Am7                 Em7\n";
+    echo "may nararamdaman din ako\n";
+    echo "          F\n";
+    echo "di kasi manhid na tulad mo\n";
+    echo "             Am7\n";
+    echo "alam kong sanay bumitaw\n";
+    echo "   Em7\n";
+    echo "ang isang tulad mo\n";
+    echo "Cadd9           F\n";
+    echo "lalayo na ba ako\n\n";
+
+    echo "[Chorus]\n";
+    echo "      Cadd9\n";
+    echo "pano naman ako\n";
+    echo "         Em7\n";
+    echo "nahulog na sayo\n";
+    echo "   Am7                        F\n";
+    echo "binitawan mo lang ba talaga ako\n\n";
+
+    echo "        Cadd9\n";
+    echo "pano naman ako\n";
+    echo "        Em7\n";
+    echo "naghintay ng matagal sayo\n";
+    echo "           Am7                     F\n";
+    echo "wala lang ba talaga lahat ng yon sayo\n";
+    echo "                     Cadd9  Em7 Am7 F\n";
+    echo "ano na bang gagawin ko\n\n";
+
+    echo "[Instrumental]\n";
+    echo "\n";
+    echo "\n";
+    echo "Cadd9  Em7 Am7 F\n\n";
+
+    echo "[Outro]\n";
+    echo "\n";
+    echo "Cadd9\n";
+    echo "pano naman ako\n";
+    echo "         Em7\n";
+    echo "nahulog na sayo\n";
+    echo "   Am7                        F\n";
+    echo "binitawan mo lang ba talaga ako\n\n";
+
+    echo "        Cadd9\n";
+    echo "pano naman ako\n";
+    echo "        Em7\n";
+    echo "naghintay ng matagal sayo\n";
+    echo "           Am7                     F\n";
+    echo "wala lang ba talaga lahat ng yon sayo\n";
+    echo "                     Cadd9\n";
+    echo "ano na bang gagawin ko\n";
+    echo "</pre>";    
+
+?>
+
+
+</body>
+</html>
