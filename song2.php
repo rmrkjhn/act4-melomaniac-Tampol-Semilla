3ebbcddbb77acc7a6c89c748b564e07e1d1b1565
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:09:35 +0800

    song2.php

---
+++ song2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Second Song</title>
+</head>
+<body>
+   <h1>Just the Way You Are by Bruno Mars</h1>
+<?php
+    echo "<pre>";
+    echo "[Verse 1]\n";
+    echo "         G\n";
+    echo "Oh, her eyes, her eyes\n";
+    echo "            D\n";
+    echo "Make the stars look like they're not shinin'\n";
+    echo "               Em\n";
+    echo "Her hair, her hair\n";
+    echo "               C\n";
+    echo "Falls perfectly without her trying\n";
+    echo "         G\n";
+    echo "She's so beautiful\n";
+    echo "                  D\n";
+    echo "And I tell her everyday\n\n";
+
+    echo "[Chorus]\n";
+    echo "           C\n";
+    echo "Yeah, I know, I know\n";
+    echo "          G\n";
+    echo "When I compliment her, she won't believe me\n";
+    echo "            Em\n";
+    echo "And it's so, it's so\n";
+    echo "            C\n";
+    echo "Sad to think that she don't see what I see\n";
+    echo "           G\n";
+    echo "But every time she asks me, 'Do I look okay?'\n";
+    echo "                 D\n";
+    echo "I say...\n\n";
+
+    echo "[Refrain]\n";
+    echo "G\n";
+    echo "When I see your face\n";
+    echo "Em\n";
+    echo "There's not a thing that I would change\n";
+    echo "C                  G\n";
+    echo "'Cause you're amazing\n";
+    echo "                D\n";
+    echo "Just the way you are\n\n";
+
+    echo "[Verse 2]\n";
+    echo "        G\n";
+    echo "And when you smile\n";
+    echo "                    D\n";
+    echo "The whole world stops and stares for awhile\n";
+    echo "             Em\n";
+    echo "'Cause girl, you're amazing\n";
+    echo "                  C\n";
+    echo "Just the way you are\n\n";
+?>
+
+
+</body>
+</html>
