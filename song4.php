2128a0321ce193d2b107c89fe830c0fd97496d94
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:11:10 +0800

    song4.php

---
+++ song4.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Fourth Song</title>
+</head>
+<body>
+   <h1>Hotel California by Eagles</h1>
+<?php
+   echo "<pre>";
+    echo "[Verse 1]\n";
+    echo "Bm         F#            A         E\n";
+    echo "On a dark desert highway, cool wind in my hair\n";
+    echo "A             E              G             D\n";
+    echo "Warm smell of colitas, rising up through the air\n";
+    echo "G         D            Em         F#\n";
+    echo "Up ahead in the distance, I saw a shimmering light\n";
+    echo "Em            F#                Bm         F#\n";
+    echo "My head grew heavy and my sight grew dim\n";
+    echo "Bm        F#        A           E\n";
+    echo "I had to stop for the night\n\n";
+
+    echo "[Verse 2]\n";
+    echo "Bm         F#            A         E\n";
+    echo "There she stood in the doorway, I heard the mission bell\n";
+    echo "A             E              G             D\n";
+    echo "And I was thinking to myself, this could be heaven or this could be hell\n";
+    echo "G         D            Em         F#\n";
+    echo "Then she lit up a candle, and she showed me the way\n";
+    echo "Em            F#                Bm         F#\n";
+    echo "There were voices down the corridor, I thought I heard them say\n\n";
+
+    echo "[Chorus]\n";
+    echo "G                      D              F#              Bm\n";
+    echo "Welcome to the Hotel California\n";
+    echo "G                         D                 F#           Bm\n";
+    echo "Such a lovely place, such a lovely face\n";
+    echo "G                         D                  F#              Bm\n";
+    echo "Plenty of room at the Hotel California\n";
+    echo "Em                            F#                 Bm\n";
+    echo "Any time of year, you can find it here\n\n";
+
+    echo "[Verse 3]\n";
+    echo "Bm         F#            A         E\n";
+    echo "Her mind is Tiffany twisted, she got the Mercedes bends, uh!\n";
+    echo "A             E              G             D\n";
+    echo "She got a lot of pretty, pretty boys that she calls friends\n";
+    echo "G           D              Em               F#\n";
+    echo "How they dance in the courtyard, sweet summer sweat\n";
+    echo "Bm          F#            A             E\n";
+    echo "Some dance to remember, some dance to forget\n";
+    echo "Bm         F#           A            E\n";
+    echo "So I called up the captain, 'Please bring me my wine,' he said\n";
+    echo "A          E             G              D\n";
+    echo "'We haven't had that spirit here since nineteen sixty-nine'\n";
+    echo "G         D               Em          F#\n";
+    echo "And still those voices are calling from far away\n";
+    echo "Bm        F#          A          E\n";
+    echo "Wake you up in the middle of the night, just to hear them say\n\n";
+
+    echo "[Chorus]\n";
+    echo "G                      D              F#              Bm\n";
+    echo "Welcome to the Hotel California\n";
+    echo "G                         D                 F#           Bm\n";
+    echo "Such a lovely place, such a lovely face\n";
+    echo "G                         D                  F#              Bm\n";
+    echo "Livin' it up at the Hotel California\n";
+    echo "Em                            F#                 Bm\n";
+    echo "What a nice surprise, bring your alibis\n\n";
+
+    echo "[Verse 4]\n";
+    echo "Bm           F#            A            E\n";
+    echo "Mirrors on the ceiling, the pink champagne on ice\n";
+    echo "A           E             G            D\n";
+    echo "'And she said, 'We are all just prisoners here, of our own device'\n";
+    echo "G           D            Em               F#\n";
+    echo "And in the master's chambers, they gathered for the feast\n";
+    echo "Bm           F#            A           E\n";
+    echo "They stab it with their steely knives, but they just can't kill the beast\n\n";
+
+    echo "[Outro]\n";
+    echo "Bm           F#            A           E\n";
+    echo "Last thing I remember, I was running for the door\n";
+    echo "A           E             G             D\n";
+    echo "I had to find the passage back to the place I was before\n";
+    echo "G              D             Em            F#\n";
+    echo "'Relax,' said the night man, 'We are programmed to receive\n";
+    echo "Bm              F#             A            E\n";
+    echo "You can check out anytime you like, but you can never leave'\n";
+    echo "</pre>";
+
+?>
+
+
+</body>
+</html>
