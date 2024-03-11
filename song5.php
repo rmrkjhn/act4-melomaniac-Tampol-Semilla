22d6b2a0f62f0f42c1888b6e1e19467a659fe98a
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:11:46 +0800

    song5.php

---
+++ song5.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Fifth Song</title>
+</head>
+<body>
+   <h1>Dynamite by BTS</h1>
+<?php
+   echo "<pre>";
+    echo "Intro: C#m\n\n";
+    echo "C#m A B G# A c#m G# x2\n\n";
+
+    echo "[Verse 1]\n";
+    echo "C#m                             A\n";
+    echo "It's been said and done\n";
+    echo "B                        G#               A            C#m      G#\n";
+    echo "Every beautiful thought\n";
+    echo "Em         A              D\n";
+    echo "'Cause ah-ah, I'm in the stars tonight\n";
+    echo "Bm         Em             A            D\n";
+    echo "So watch me bring the fire and set the night alight\n\n";
+
+    echo "[Verse 2: Jungkook]\n";
+    echo "Bm\n";
+    echo "Shoes on, get up in the morn'\n";
+    echo "      Em\n";
+    echo "Cup of milk, let's rock and roll\n";
+    echo "A\n";
+    echo "King Kong, kick the drum\n";
+    echo "        D\n";
+    echo "Rolling on like a Rolling Stone\n";
+    echo "Bm\n";
+    echo "Sing song when I'm walking home\n";
+    echo "     Em\n";
+    echo "Jump up to the top, LeBron\n";
+    echo "A\n";
+    echo "Ding-dong, call me on my phone\n";
+    echo "    D\n";
+    echo "Ice tea and a game of ping pong\n";
+    echo "Em       A         D\n";
+    echo "'Cause ah-ah, I'm in the stars tonight\n";
+    echo "Bm      Em          A           D\n";
+    echo "So watch me bring the fire and set the night alight (Hey)\n\n";
+
+    echo "[Verse 3: RM, j-hope]\n";
+    echo "Bm\n";
+    echo "This is getting heavy\n";
+    echo "        Em\n";
+    echo "Can you hear the bass boom? I'm ready\n";
+    echo "A\n";
+    echo "Life is sweet as honey\n";
+    echo "           D\n";
+    echo "Yeah, this beat cha-ching like money\n";
+    echo "Bm                  Em\n";
+    echo "Disco overload, I'm into that, I'm good to go\n";
+    echo "A\n";
+    echo "I'm diamond, you know I glow up\n";
+    echo "D\n";
+    echo "Hey, so let's go\n";
+    echo "Em         A             D\n";
+    echo "'Cause ah-ah, I'm in the stars tonight\n";
+    echo "Bm        Em           A          D\n";
+    echo "So watch me bring the fire and set the night alight (Hey)\n\n";
+
+    echo "[Chorus: Jungkook, Jimin]\n";
+    echo "Bm            Em         A             D\n";
+    echo "'Cause ah-ah, I'm in the stars tonight\n";
+    echo "            Bm        Em             A            D\n";
+    echo "So watch me bring the fire and set the night alight (Hey)\n\n";
+    echo "C#m                 F#m        B               E\n";
+    echo "Shining through the city with a little funk and soul\n";
+    echo "        C#m      F#m     B           E\n";
+    echo "So I'ma light it up like dynamite, woah (Light it up like dynamite)\n";
+    echo "\n";
+    echo "\n";
+    echo "[Post-Chorus: Jungkook, Jimin, V]\n";
+    echo "C#m    F#m      B           E\n";
+    echo "Dyn-n-n-n-na-na-na, life is dynamite (Life is dynamite)\n";
+    echo "C#m    F#m      B           E\n";
+    echo "Dyn-n-n-n-na-na-na, life is dynamite\n";
+    echo "C#m                 F#m         B               E\n";
+    echo "Shining through the city with a little funk and soul\n";
+    echo "        C#m      F#m     B           E\n";
+    echo "So I'ma light it up like dynamite, woah\n";
+?>
+
+
+</body>
+</html>
