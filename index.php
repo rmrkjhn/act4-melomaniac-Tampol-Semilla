b7c06d26a1c9ba10d7e3b8705c7f70b073990db4
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:08:38 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Melomaniac PlayList</title>
+</head>
+<body style="background-color: red;">
+    <h1>Welcome To My Melomaniac</h1>
+    <p>Explore our collection of songs:</p>
+    <ul>
+        <li><a href="song1.php">Pano by Zack Tabudlo</a></li>
+        <li><a href="song2.php">Just the Way You Are by Bruno Mars</a></li>
+        <li><a href="song3.php">Oks Lang Ako by Jroa</a></li>
+        <li><a href="song4.php">Hotel California by Eagles</a></li>
+        <li><a href="song5.php">Dynamite by BTS</a></li>
+    </ul>
+</body>
+</html>
+  
