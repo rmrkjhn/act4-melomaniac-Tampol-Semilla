958cc79512940614878483d0395665b13826b3ed
TampolRemarkJohn <remarktampol@gmail.com>
2024-03-11 18:10:03 +0800

    song3.php

---
+++ song3.php
@@ -0,0 +1,87 @@
+<html>
+   <head>
+      <title>Third Song</title>
+</head>
+<body>
+   <h1>Oks Lang Ako by Jroa</h1>
+<?php
+   echo "<pre>";
+    echo "Saan na 'to patungo\n";
+    echo "Cadd9           G\n";
+    echo "Hindi ko na kasi alam\n";
+    echo "Cadd9           G\n";
+    echo "Hinahanap ang sagot sa bakit\n";
+    echo "Cadd9           G\n";
+    echo "Hindi ko na kasi alam\n";
+    echo "Cadd9           G\n";
+    echo "Hindi ka na nakikinig\n";
+    echo "Cadd9           G\n";
+    echo "Hindi ka na kinikilig\n";
+    echo "Cadd9           G\n";
+    echo "Hindi ka na natutuwa\n";
+    echo "Cadd9           G\n";
+    echo "Pag may pasalubong na isaw\n\n";
+
+    echo "[Chorus]\n\n";
+
+    echo "Em      Cadd9      G      D\n";
+    echo "Nagbago na ang lahat sa’yo\n";
+    echo "Em      Cadd9             G       D\n";
+    echo "Nagbago na ang lahat pati ang tayo\n";
+    echo "Em              Cadd9\n";
+    echo "Nagbago na ang ‘yong tingin\n";
+    echo "     G                 D        Cadd9\n";
+    echo "Ang ‘yong ngiti, ang ‘yong nararamdaman\n";
+    echo "            D\n";
+    echo "Ang gusto ko lang naman\n";
+    echo "Cadd9             G\n";
+    echo "Ay yakapin mo ako\n";
+    echo "Cadd9           G\n";
+    echo "Kahit hindi na totoo\n";
+    echo "Cadd9                G\n";
+    echo "Maiintindihan naman kita\n";
+    echo "Cadd9                             G\n";
+    echo "Kung sawa ka na, kung san ka sasaya\n";
+    echo "             Am D\n";
+    echo "Wag kang mag-alala\n";
+    echo "           Cadd9  G\n";
+    echo "Oks lang ako\n\n";
+
+    echo "[2nd Verse]\n\n";
+
+    echo "Cadd9             G\n";
+    echo "Oy salamat nga pala\n";
+    echo "Cadd9            G\n";
+    echo "Sa mga sandali nating masaya\n";
+    echo "Cadd9              G\n";
+    echo "Unti-unti na rin akong bibitaw\n";
+    echo "     Cadd9                G\n";
+    echo "Kahit ako na lang ang sasayaw\n";
+    echo "      Em    Cadd9      G     D\n";
+    echo "Kasi malabo na ang lahat sayo\n";
+    echo "Em     Cadd9           G       D\n";
+    echo "Malabo na ang lahat pati ang tayo\n";
+    echo "Em             Cadd9\n";
+    echo "Malabo na ang ‘yong tingin\n";
+    echo "     G                 D        Cadd9\n";
+    echo "Ang ‘yong ngiti, ang ‘yong nararamdaman\n";
+    echo "            D\n";
+    echo "Ang gusto ko lang naman\n";
+    echo "Cadd9           G\n";
+    echo "Ay yakapin mo ako\n";
+    echo "Cadd9           G\n";
+    echo "Kahit hindi na totoo\n";
+    echo "Cadd9                G\n";
+    echo "Maiintindihan naman kita\n";
+    echo "Cadd9                             G\n";
+    echo "Alam kong sawa ka na, dun ka na sa masaya\n";
+    echo "             Am  D\n";
+    echo "Wag kang mag-alala\n";
+    echo "            C  G\n";
+    echo "Oks lang ako\n";
+    
+?>
+
+
+</body>
+</html>
